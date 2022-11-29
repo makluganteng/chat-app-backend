31328d15bcb71252a28a9aa2a400ad364805e915
makluganteng11 <vincentowent1310@gmail.com>
2022-11-29 21:01:34 +0800

    merge fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
+import { AppGatewayGateway } from './app-gateway/app-gateway.gateway';
 
 @Module({
   imports: [UsersModule, DatabaseModule, AuthModule],
   controllers: [],
-  providers: [],
+  providers: [AppGatewayGateway],
 })
-export class AppModule { }
+export class AppModule {}
